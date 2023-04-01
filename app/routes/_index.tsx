f24187733cfc7274c58543b706e3e55495688c21
Peter Garside-Quin <petegq@gmail.com>
2023-04-01 19:32:15 +0100

    Add types

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,13 @@
 import { useLoaderData } from "@remix-run/react";
 import supabase from "utils/supabase";
+import type { LoaderArgs } from "@remix-run/node";
 
-export const loader = async () => {
+export const loader = async ({}: LoaderArgs) => {
   const { data } = await supabase.from("messages").select();
-  return data;
+  return { messages: data ?? [] };
 };
 
 export default function Index() {
-  const { data } = useLoaderData();
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  const { messages } = useLoaderData<typeof loader>();
+  return <pre>{JSON.stringify(messages, null, 2)}</pre>;
 }
